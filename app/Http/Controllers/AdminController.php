3543d5926939e1f64841c5435924c9a94876bc46
magiksen <elemejore@gmail.com>
2023-09-03 11:56:50 -0400

    Subida del proyecto

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        $notification = array(
+            'message' => 'Has salido correctamente',
+            'alert-type' => 'success'
+        );
+
+        return redirect('/login')->with($notification);
+    }
+
+}
