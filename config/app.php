6ebc4e5f9ce4a136d41aa83e1bd5863b32ccf653
magiksen <elemejore@gmail.com>
2023-09-24 14:04:46 -0400

    Instalación de Spatie para permisos

--- config/app.php
+++ config/app.php
@@ -168,6 +168,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        // Spatie permisos
+        Spatie\Permission\PermissionServiceProvider::class,
     ])->toArray(),
 
     /*
